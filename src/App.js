7f60e895e200e57f70040956e8770a9681e39278
PaulTA91 <parmst202@caledonian.ac.uk>
2022-11-26 15:01:56 +0000

    Navigation added (see lab7)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./Components/Home";
+import AboutUs from "./Components/AboutUs";
+import Navigation from "./Components/Navigation";
+import NoPage from "./Components/NoPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Introductory Text</p>
+      <p>Banner image/carousel here too?</p>
+    </>
+  );
+};
+
+export default Home;
