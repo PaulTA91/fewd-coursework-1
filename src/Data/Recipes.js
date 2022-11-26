de36dd39a1e130bd8d56b0bf4250eb4f63437dfe
PaulTA91 <parmst202@caledonian.ac.uk>
2022-11-26 14:51:09 +0000

    bsaic recipe data added

---
+++ src/Data/Recipes.js
@@ -0,0 +1,614 @@
+export const recipes = [
+  {
+    id: "1",
+    name: "Carrot Cake",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Mushroom Risotto",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Chocolate Cake",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "4",
+    name: "Spaghetti Bolognese",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "5",
+    name: "Tomato Soup",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "6",
+    name: "Garlic Bread",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "7",
+    name: "Baked Potato",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "8",
+    name: "Huevos Rancheros",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "9",
+    name: "Slow Cooker Labm Curry",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "10",
+    name: "Cheese Cake",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "11",
+    name: "Fish and Chips",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "12",
+    name: "Lasagna",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "13",
+    name: "Spiced Carrot and Lentil Soup",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "14",
+    name: "Shakshuka",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "15",
+    name: "Easy Chicken Soup",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "16",
+    name: "Vegetarian Ramen",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+  {
+    id: "17",
+    name: "Winter Vegetable Pie",
+    description:
+      "Classic carrot cake with moreish icing and chopped walnuts or pecans.",
+    category: "Dessert",
+    instructions:
+      "Mix the sugar and oil. Add eggs, beating after each one. Add carrots. Sift the dry ingredients together, and stir them in. Butter a 9x13 pan, pour in the batter and bake at 325F for 40 minutes. Top with Cream Cheese Frosting for Carrot Cake.",
+    ingredients: {
+      "Vegetable Oil": "230ml",
+      "Natural Yogurt": "100g",
+      "Large Eggs": "4",
+      "Vanilla Extract": "1.p tsp",
+      "Orange Zest": "1/2",
+      "Self-raising Flour": "256g",
+      "Light Muscovado Sugar": "335g",
+      "Ground Cinnamon": "2.5 tsp",
+      "Grated Nutmeg": "5g",
+      "Grated Carrots": "265g",
+      "Chopped Walnuts": "100g",
+      "Salted Butter": "100g",
+      "Icing Sugar": "300g",
+      "Soft Cheese": "100g",
+    },
+
+    reviews: [
+      {
+        name: "username",
+        content: "Review content here",
+      },
+      {
+        name: "username2",
+        content: "Review 2 content here",
+      },
+    ],
+  },
+];
